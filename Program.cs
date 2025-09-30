4eac0470b34a2cf1f681f2e76b9361ff44c16032
Razvan <termita@gmail.com>
2025-09-30 19:44:50 +0800

    updated copy to also work on macos and linux

--- Program.cs
+++ Program.cs
@@ -377,12 +377,43 @@ class Program
         {
             var code = TotpGenerator.GenerateCode(accounts[visibleCodeIndex].Secret);
 
-            // Use Windows clipboard API
+            // Detect operating system and use appropriate clipboard command
+            string clipboardCommand;
+            if (OperatingSystem.IsWindows())
+            {
+                clipboardCommand = "clip";
+            }
+            else if (OperatingSystem.IsMacOS())
+            {
+                clipboardCommand = "pbcopy";
+            }
+            else if (OperatingSystem.IsLinux())
+            {
+                // Try xclip first, fall back to xsel if not available
+                clipboardCommand = "xclip";
+                if (!IsCommandAvailable("xclip"))
+                {
+                    if (IsCommandAvailable("xsel"))
+                    {
+                        clipboardCommand = "xsel";
+                    }
+                    else
+                    {
+                        return false; // No clipboard utility available
+                    }
+                }
+            }
+            else
+            {
+                return false; // Unsupported operating system
+            }
+
             var process = new System.Diagnostics.Process
             {
                 StartInfo = new System.Diagnostics.ProcessStartInfo
                 {
-                    FileName = "clip",
+                    FileName = clipboardCommand,
+                    Arguments = GetClipboardArguments(clipboardCommand),
                     RedirectStandardInput = true,
                     UseShellExecute = false,
                     CreateNoWindow = true
@@ -400,6 +431,43 @@ class Program
         {
             return false;
         }
+    }
+
+    static bool IsCommandAvailable(string command)
+    {
+        try
+        {
+            var process = new System.Diagnostics.Process
+            {
+                StartInfo = new System.Diagnostics.ProcessStartInfo
+                {
+                    FileName = "which",
+                    Arguments = command,
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                }
+            };
+
+            process.Start();
+            process.WaitForExit();
+
+            return process.ExitCode == 0;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    static string GetClipboardArguments(string command)
+    {
+        return command switch
+        {
+            "xclip" => "-selection clipboard",
+            "xsel" => "--clipboard --input",
+            _ => ""
+        };
     }
 
     static void ShowTemporaryMessage(string message)
